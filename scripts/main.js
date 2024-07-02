c7b415eb5e73085677d122497b38de1d23df25cb
Robson Niemeyer <nierobs@hotmail.com>
2024-07-01 22:36:19 -0300

    Add animation on testimonial section

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,15 @@
 'use strict'
 
-window.addEventListener('load', () => {
-  //
-};
+const animation = document.getElementsByClassName('animation');
+
+for (let i = 0; i < animation.length; i++) {
+    animation[i].style.opacity = 0;
+}
+
+window.addEventListener('scroll', (event) => {
+    for (let i = 0; i < animation.length; i++) {
+        if (window.innerHeight + window.scrollY - animation[i].offsetTop > 0) {
+            animation[i].classList.add('fadein');
+        }
+    }
+});
